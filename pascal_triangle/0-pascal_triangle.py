0c1fee4bf7d14ec9f22b6e626a8fbffc2cc8354e
kennyg37 <k.ganza@alustudent.com>
2023-10-20 17:26:25 +0200

    initial commit

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+''' Function that creates Pascal Triangle '''
+
+
+def pascal_triangle(n):
+
+    '''returning empty list if n is less than or equal to 0'''
+    if n <= 0:
+        return []
+
+    this_triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(this_triangle[i-1][j-1] + this_triangle[i-1][j])
+        row.append(1)
+        this_triangle.append(row)
+
+    return this_triangle
